7500709087923a08dc7d2c8571b26f2a1c9bd549
Vincenzo Cuccia <v.cuccia@almaviva.it>
2022-12-04 12:05:42 +0100

    Initial commit

---
+++ ShopOnline.Api/Entity/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopOnline.Api.Entity;
+
+public class CartItem
+{
+    public int Id { get; set; }
+    public int CartId { get; set; }
+    public int ProductId { get; set; }
+    public int Qty { get; set; }
+}
